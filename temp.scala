b2eb44c53cd700d53156739490754989f46dde90
Nick DeRobertis <whoopnip@gmail.com>
2021-04-11 08:36:17 -0400

    Add project structure from sbt new playframework/play-scala-seed.g8 and dockerize properly. Now can access server

---
+++ temp.scala
@@ -0,0 +1,14 @@
+package temp
+
+// def foo(x: Int): Int = {
+//   val anonFunc: Int => Int = { z =>
+//     if (z > 5)
+//       return z // This line makes z the return value of foo!
+//     else
+//       z + 2    // This line is the return value of anonFunc
+//   }
+//   anonFunc(x) + 10  // This line is the return value of foo
+// }
+
+val s = Set(1, 3, 5)
+s
